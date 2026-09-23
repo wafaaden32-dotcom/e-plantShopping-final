c8c4760081704f53264c6024f730a84d2d3cf8fc
wafaaden32-dotcom <wafaaden32@gmail.com>
2026-09-23 17:18:30 +0400

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,40 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const existingItem = state.items.find(
+        (item) => item.name === action.payload.name
+      );
 
-    
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          name: action.payload.name,
+          image: action.payload.image,
+          cost: action.payload.cost,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
     },
   },
 });
